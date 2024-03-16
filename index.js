ca66a4f6c53c93072ae56089a8a56e686b6a592c
vanshjangir <vanshjangir0001@gmail.com>
2024-03-16 18:21:54 +0530

    second

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-
+const GoogleGenerativeAI = require('@google/generative-ai').GoogleGenerativeAI;
 const express = require('express')
 const app = express()
 const port = 3000
+const CONFIG = require('./config.json');
+const API_KEY = CONFIG.genapi
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+async function callgemini() {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+  let preprompt = "";
+
+  let prompt = "give me some fashion categories for indian occasions";
+  prompt = prompt+preprompt;
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  return text;
+}
+
+app.get('/categories', async (req, res) => {
+  res.send(await callgemini());
 })
 
 app.listen(port, () => {
