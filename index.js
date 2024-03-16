f8d52478ffdee1aee3a9a0c1493587ed03b6e973
vanshjangir <vanshjangir0001@gmail.com>
2024-03-16 19:18:44 +0530

    added support for keywords

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const CONFIG = require('./config.json');
-const API_KEY = CONFIG.genapi
+const GEMINI_API_KEY = CONFIG.genapi
 
 
-const genAI = new GoogleGenerativeAI(API_KEY);
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 async function callgemini(prompt) {
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  let preprompt = "give output in single string like this [\"firstidea\", \"second idea\"] with ideas as strings ";
+  let preprompt = "give output in single string like this [[\"firstidea\", \"keywords\"], [\"second idea\", \"keywords\"]] with ideas as strings and keywords will be some keywords related to that idea ";
   prompt = prompt+preprompt;
 
   const result = await model.generateContent(prompt);
@@ -20,12 +20,14 @@ async function callgemini(prompt) {
   return text;
 }
 
+
 app.get('/categories', async (req, res) => {
   const prompt = "give me some fashion categories for indian occasions just as single word for each category with only english alphabets";
 
   const response = await callgemini(prompt);
   const array = JSON.parse(response);
   console.log(array);
+
   res.send(array);
 })
 
