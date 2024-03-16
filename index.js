559ea0db4b8486b5ddb8c4f1bbb781b94a904c32
vanshjangir <vanshjangir0001@gmail.com>
2024-03-16 21:56:00 +0530

    switch to 30 words

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
-const {MongoClient, GridFSStream} = require('mongodb')
+const {MongoClient} = require('mongodb')
 const uri = "mongodb+srv://hackOharbour:D1kVfg4XSaaq3sUX@cluster0.g1ic6ja.mongodb.net/?retryWrites=true&w=majority";
 const GEMINI_API_KEY = "AIzaSyBrjP4-mCYNeLUqEj0wyFFLGARahdUrA5c";
 
@@ -18,7 +18,7 @@ const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 async function callgemini(prompt) {
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  let preprompt = "give output in about 30 words in a single paragraph with a fashion ideas for how to dress according to the following occasion\n";
+  let preprompt = "give output in about 30 words in a single string in this JSON style format on key(para) for single paragraph about to dress for the following occasion, and another key(keywords) for a single string of keywords about the dresses and clothes without spaces and separated by commas,  with a fashion ideas for how to dress according to the following occasion\n";
   prompt = prompt+preprompt;
 
   const result = await model.generateContent(prompt);
@@ -91,7 +91,8 @@ app.get('/searches', async (req, res) => {
   
   const prompt = req.body;
   let result= await callgemini(prompt['prompt']);
-  res.status(200).json({"result":result});
+  result = JSON.parse(result);
+  res.status(200).json({"result":result.para, "keywords": result.keywords});
   
 })
 app.listen(port, () => {
